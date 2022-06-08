9a2f0b5d305b37b46b3bae81c2a3a55558f2a5da
QuangTruong224 <changtraitre2000@gmail.com>
2022-06-08 19:38:40 +0700

    push

---
+++ ss3/bai_tap/caculato/src/app/calculator/calculator.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  first = 0;
+  second = 0;
+  operator = '-';
+  result = '';
+
+  addition() {
+    this.operator = '+';
+  }
+
+  subtraction() {
+    this.operator = '-';
+  }
+
+  multiplication() {
+    this.operator = '*';
+  }
+
+  division() {
+    this.operator = '/';
+  }
+
+  calcutator() {
+    switch (this.operator) {
+
+      case '+':
+        this.result = String(this.first + this.second);
+        break;
+      case '-':
+        this.result = String(this.first - this.second);
+        break;
+      case '*':
+        this.result = String(this.first * this.second);
+        break;
+      case '/':
+        // tslint:disable-next-line:triple-equals
+        if (this.second == 0) {
+          this.result = 'Không dc là số 0';
+          break;
+        } else {
+          this.result = String(this.first / this.second);
+          break;
+        }
+    }
+    return this.result;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

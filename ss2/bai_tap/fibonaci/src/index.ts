91f551522e09a5173b1c1d3d5c0e6e6d00af0a25
QuangTruong224 <changtraitre2000@gmail.com>
2022-06-08 21:47:21 +0700

    push

--- ss2/bai_tap/fibonaci/src/index.ts
+++ ss2/bai_tap/fibonaci/src/index.ts
@@ -1,18 +1,19 @@
-function generateFibonacci(num: number) {
-    let fibonacciArr = [1, 2];
-    let a: number = 0
+function generateFibonacci(num:number):number[] {
+    let fibonacciArr:number[] = [];
+    let fibonacciNum:number = 1;
+    let temp:number = 0;
     for (let i = 0; i < num; i++) {
-        a = fibonacciArr[i + 1] + fibonacciArr[i]
-        fibonacciArr.push(a)
+        fibonacciArr.push(fibonacciNum)
+        fibonacciNum += temp;
+        temp = fibonacciNum - temp;
     }
     return fibonacciArr;
 }
 
-let fibonacciArr = generateFibonacci(4);
-//1 1 2 3 5 8 13 21
-let sum = 0;
+let fibonacciArr = generateFibonacci(3);
+let sum: number = 0;
 for (let i = 0; i < fibonacciArr.length; i++) {
     console.log(fibonacciArr[i]);
     sum += fibonacciArr[i];
 }
-console.log('Tổng ' + fibonacciArr.length + ' số fibonacci đầu tiên là: ' + sum);
+console.log('Tổng '+fibonacciArr.length+' số fibonacci đầu tiên là: '+sum);

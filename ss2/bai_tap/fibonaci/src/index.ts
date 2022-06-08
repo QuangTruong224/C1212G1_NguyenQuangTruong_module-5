728859ff68d1267a90c4820c423f3cf61a8a3f69
QuangTruong224 <changtraitre2000@gmail.com>
2022-06-08 09:15:12 +0700

    push

--- ss2/bai_tap/fibonaci/src/index.ts
+++ ss2/bai_tap/fibonaci/src/index.ts
@@ -1,17 +1,17 @@
 function generateFibonacci(num: number) {
-    var fibonacciArr = [];
-    var fibonacciNum = 1;
-    var temp = 0;
-    for (var i = 0; i < num; i++) {
-        fibonacciNum += temp;
-        fibonacciArr.push(fibonacciNum);
-        temp = fibonacciNum - temp;
+    let fibonacciArr = [1, 2];
+    let a: number = 0
+    for (let i = 0; i < num; i++) {
+        a = fibonacciArr[i + 1] + fibonacciArr[i]
+        fibonacciArr.push(a)
     }
     return fibonacciArr;
 }
-var fibonacciArr = generateFibonacci(10);
-var sum = 0;
-for (var i = 0; i < fibonacciArr.length; i++) {
+
+let fibonacciArr = generateFibonacci(4);
+//1 1 2 3 5 8 13 21
+let sum = 0;
+for (let i = 0; i < fibonacciArr.length; i++) {
     console.log(fibonacciArr[i]);
     sum += fibonacciArr[i];
 }

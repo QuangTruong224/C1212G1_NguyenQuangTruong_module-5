fbe465662e837e05a4a3af6b47ec98d7fdeec564
QuangTruong224 <changtraitre2000@gmail.com>
2022-06-09 09:39:47 +0700

    push case

--- angular-case-study/src/app/service-edit/service-edit.component.ts
+++ angular-case-study/src/app/service-edit/service-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-service-edit',
-  templateUrl: './service-edit.component.html',
+  templateUrl:'./service-edit.component.html',
   styleUrls: ['./service-edit.component.css']
 })
 export class ServiceEditComponent implements OnInit {

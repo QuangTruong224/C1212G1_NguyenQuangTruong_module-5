ef22c3018b434d920bb4c25e0544f6256c4ee590
QuangTruong224 <changtraitre2000@gmail.com>
2022-06-13 17:32:17 +0700

    push

---
+++ ss6/bai_tap/form-dang-ki/src/app/register/register.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor() {
+    this.registerForm = new FormGroup({
+      gmail: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      country: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required, Validators.min(18)]),
+      phone: new FormControl('', [Validators.required, Validators.pattern('^\\+84\\d{9,10}$')]),
+      gender: new FormControl('', [Validators.required])
+    }, [this.customPassword]);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.registerForm);
+  }
+
+  customPassword(confirm: AbstractControl) {
+    const password = confirm.get('password').value;
+    const confirmPassword = confirm.get('confirmPassword').value;
+    if (confirmPassword !== password) {
+      return {
+        confirmPassword: true
+      };
+    } else {
+      return null;
+    }
+  }
+
+
+}
